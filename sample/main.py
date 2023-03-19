86eac8ae521aa8e8f1a08fe643ab1a1679db62e3
Sayar1106 <sayarbanerjee@gmail.com>
2023-03-19 16:02:04 -0700

    Cleanup

--- sample/main.py
+++ sample/main.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import time
-from url import map_dict
+from url import (
+map_dict, URL_FEEDER, URL_FEEDER_SUBPAGE, 
+ URL_CLEANER, URL_DOOR, URL_DOOR_SUBPAGE
+)
 from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 import os
@@ -28,26 +31,21 @@ def main():
     # Classification of companies and products. Tier vs Ingredients
     # Does top tier company have more nutritional products as compared to bottom tier.
     # Define tier based on price. Do some ingredient analysis based on tier
-    url_feeder = "https://www.chewy.com/s?query=automatic+feeders&rh=price_d%3A5" # filter out price < $50 - other animal or not automatic feeders
-    url_feeder_subpage = "https://www.chewy.com/s?query=automatic+feeders&page={}&rh=price_d%3A5"
-    url_cleaner = "https://www.chewy.com/s?query=automatic%20self-cleaning"
-    url_door = "https://www.chewy.com/s?query=automatic+doors&rh=price_d%3A5" # filter out price < $50
-    url_door_subpage = "https://www.chewy.com/s?query=automatic+doors&page={}&rh=price_d%3A5"
     scraper = PetTechScraper()
     scraper.tech = 'chewy_tech_cleaner'
-    scraper.saveResultPage(url_cleaner, "", "tech_cleaners")
+    scraper.saveResultPage(URL_CLEANER, "", "tech_cleaners")
 
 
     # create a new db for feeders
     # tech = db["chewy_tech_feeder"]
     scraper.tech = 'chewy_tech_feeder'
-    scraper.saveResultPage(url_feeder, url_feeder_subpage, "tech_feeders")
+    scraper.saveResultPage(URL_FEEDER, URL_FEEDER_SUBPAGE, "tech_feeders")
 
 
     # create a new db for doors
     # tech = db["chewy_tech_door"]
     scraper.tech = 'chewy_tech_door'
-    scraper.saveResultPage(url_door, url_door_subpage, "tech_doors")
+    scraper.saveResultPage(URL_DOOR, URL_DOOR_SUBPAGE, "tech_doors")
 
 if __name__ == "__main__":
     main()
